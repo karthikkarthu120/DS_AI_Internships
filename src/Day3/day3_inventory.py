1ea36728c01c8afb5b21d7a82b9e4ff80641243a
karthikkarthu120 <karthikkarthu120@gmail.com>
2026-02-04 10:56:32 +0530

    adding day3 files to git

---
+++ src/Day3/day3_inventory.py
@@ -0,0 +1,8 @@
+inventory = ["Apples", "Bananas", "Carrots", "Dates"]
+print(inventory)
+inventory.append("Eggs")
+print("Updated Inventory After Adding Eggs :\n",inventory)
+inventory.remove("Bananas")
+inventory.sort()
+print("Final Inventory: ",inventory)
+
